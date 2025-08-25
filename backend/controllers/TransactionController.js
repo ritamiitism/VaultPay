a1828acb2a4ad4e70efeb372e87b31543d6ec7b4
RK <142689034+ritamiitism@users.noreply.github.com>
2025-08-25 19:55:43 +0530

    Update TransactionController.js

--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -1,44 +1,60 @@
+const mongoose = require("mongoose");
 const Transaction = require("../models/Transaction");
 const Wallet = require("../models/Wallet");
 const User = require("../models/User");
 
 const transferMoney = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
-    const senderWallet = await Wallet.findOne({ user: req.params.userId });
-    const recipientWallet = await Wallet.findOne({ user: req.body.toAccount });
+    const senderWallet = await Wallet.findOne({ user: req.params.userId }).session(session);
+    const recipientWallet = await Wallet.findOne({ user: req.body.toAccount }).session(session);
+
+    if (!senderWallet || !recipientWallet) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json({ message: "Wallet not found!", success: false });
+    }
 
     if (senderWallet.user.toString() === recipientWallet.user.toString()) {
-      return res
-        .status(400)
-        .json({ message: "You can't transfer to yourself!", success: false });
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(400).json({ message: "You can't transfer to yourself!", success: false });
     }
 
     if (senderWallet.balance < req.body.amountToSend) {
-      return res
-        .status(400)
-        .json({ message: "Insufficient balance!", success: false });
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(400).json({ message: "Insufficient balance!", success: false });
     }
 
     const amount = Number(req.body.amountToSend);
+
+    // Update balances
     senderWallet.balance = Number(senderWallet.balance) - amount;
     recipientWallet.balance = Number(recipientWallet.balance) + amount;
 
-    await senderWallet.save();
-    await recipientWallet.save();
+    await senderWallet.save({ session });
+    await recipientWallet.save({ session });
 
-    const transaction = await Transaction.create({
+    // Record transaction
+    await Transaction.create([{
       from: req.params.userId,
       to: req.body.toAccount,
-      amount: req.body.amountToSend,
+      amount,
       transactionType: "transfer"
-    });
+    }], { session });
 
-    res
-      .status(200)
-      .json({ message: "Transaction completed successfully!", success: true });
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(200).json({ message: "Transaction completed successfully!", success: true });
   } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
     console.log(error);
-    res.status(500).send({message: error.message, success: false});
+    res.status(500).send({ message: error.message, success: false });
   }
 };
 
@@ -46,14 +62,11 @@ const getTransactions = async (req, res) => {
   try {
     const userId = req.params.userId;
 
-    // Find the user's wallet
     const user = await User.findById(userId);
-
     if (!user) {
-      return res.status(404).send({message: "User not found!", success: false});
+      return res.status(404).send({ message: "User not found!", success: false });
     }
 
-    // Find transactions involving the user
     const transactions = await Transaction.find({
       $or: [{ from: userId }, { to: userId }]
     });
@@ -61,7 +74,7 @@ const getTransactions = async (req, res) => {
     res.status(200).send(transactions);
   } catch (error) {
     console.log(error);
-    res.status(500).send({message: error.message, success: false});
+    res.status(500).send({ message: error.message, success: false });
   }
 };
 
